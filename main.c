09a1d15182e1c5c2eb7bfa2ea3ea3e78de15a388
dergachev <dergachev88@gmail.com>
2019-04-13 14:09:36 +0300

    gray-scale visualization

--- main.c
+++ main.c
@@ -31,7 +31,7 @@ typedef struct
     png_bytep * row_pointers;
 } PNGData;
 
-void process_data(PNGData* png_data)
+void load_data(PNGData* png_data)
 {
     for (int y = 0; y < png_data->height; ++y)
     {
@@ -50,12 +50,28 @@ void process_data(PNGData* png_data)
     }
 }
 
-void write_png_file(const char* file_name, PNGData* png_data)
+void write_png_file(const char* in_file_name, const char* out_file_name, PNGData* png_data)
 {
+    FILE *in = fopen(in_file_name, "rb");
+    if (!in)
+        abort_("[write_png_file] File %s could not be opened for reading", in_file_name);
+    double * arr = malloc_(sizeof(arr[0]) * png_data->width*png_data->height);
+    int res = fread(arr, sizeof(arr[0]), png_data->width*png_data->height, in);
+    if (res != png_data->width*png_data->height)
+        abort_("[write_png_file] Can't read requested data");
+    fclose(in);
+    
+    double min = arr[0], max = arr[0];
+    for (size_t idx = 0; idx < png_data->width*png_data->height; ++idx)
+    {
+        min = min < arr[idx] ? min : arr[idx];
+        max = max > arr[idx] ? max : arr[idx];
+    }
+    
     /* create file */
-    FILE *fp = fopen(file_name, "wb");
+    FILE *fp = fopen(out_file_name, "wb");
     if (!fp)
-        abort_("[write_png_file] File %s could not be opened for writing", file_name);
+        abort_("[write_png_file] File %s could not be opened for writing", out_file_name);
     
     /* initialize stuff */
     png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
@@ -78,11 +94,21 @@ void write_png_file(const char* file_name, PNGData* png_data)
     png_write_info(png_ptr, info_ptr);
     
     /* allocate buffer and fill it */
-    printf("size of row = %d\n", png_get_rowbytes(png_ptr, info_ptr));
     png_data->row_pointers = (png_bytep*) malloc_(sizeof(png_bytep) * png_data->height);
     for (int y = 0; y < png_data->height; ++y)
         png_data->row_pointers[y] = (png_byte*) malloc_(png_get_rowbytes(png_ptr, info_ptr));
-    process_data(png_data);
+    for (int y = 0; y < png_data->height; ++y)
+    {
+        png_byte* row = png_data->row_pointers[y];
+        for (int x = 0; x < png_data->width; ++x)
+        {
+            unsigned int val = (unsigned int)(((arr[y*png_data->width+x] - min) / (max - min)) * 65535);
+            png_save_uint_16(&row[8*x], val);
+            png_save_uint_16(&row[8*x+2], val);
+            png_save_uint_16(&row[8*x+4], val);
+            png_save_uint_16(&row[8*x+6], 256*256-1);
+        }
+    }
     
     /* write bytes */
     if (setjmp(png_jmpbuf(png_ptr)))
@@ -99,21 +125,22 @@ void write_png_file(const char* file_name, PNGData* png_data)
         free(png_data->row_pointers[y]);
     free(png_data->row_pointers);
     
+    free(arr);
     fclose(fp);
 }
 
 int main(int argc, char **argv)
 {
-    if (argc != 2)
-        abort_("Usage: program_name <file_out>");
+    if (argc != 3)
+        abort_("Usage: program_name <binary file in> <png file out>");
     
     PNGData png_data;
-    png_data.width = 256;
-    png_data.height = 256;
+    png_data.width = 3840;
+    png_data.height = 3840;
     png_data.color_type = PNG_COLOR_TYPE_RGBA;
     png_data.bit_depth = 16;
     
-    write_png_file(argv[1], &png_data);
+    write_png_file(argv[1], argv[2], &png_data);
     
     return 0;
 }
